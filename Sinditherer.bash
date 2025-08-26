dde656a71a1ddc896e905aee6cc80d08caec68cd
SindiKali <m4ttpizz4@gmail.com>
2025-08-26 16:36:47 +0000

    Upload do arquivo Sinditherer.bash

--- Sinditherer.bash
+++ Sinditherer.bash
@@ -1,4 +1,13 @@
 #!/bin/bash
 read -p "Digite a URL do site: " url
 echo "Pesquisando informações do domínio..."
+
 whois "$url" > whois_info.txt
+email=$(grep -i 'email:' whois_info.txt | awk '{print $2}')
+dns=$(grep -i 'Name Server: ' whois_info.txt | awk '{print $2}')
+
+echo "E-mail de contato:"
+grep -i 'email:' whois_info.txt
+
+echo "Servidores DNS:"
+grep -i 'Name Server:' whois_info.txt
