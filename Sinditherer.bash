9d87e5007b27bc4e591b1108da9e6d010f17c129
SindiKali <m4ttpizz4@gmail.com>
2025-08-27 17:40:29 +0000

    Atualizar o Sinditherer.bash

--- Sinditherer.bash
+++ Sinditherer.bash
@@ -3,8 +3,6 @@ read -p "Digite a URL do site: " url
 echo "Pesquisando informações do domínio..."
 
 whois "$url" > whois_info.txt
-email=$(grep -i 'email:' whois_info.txt | awk '{print $2}')
-dns=$(grep -i 'Name Server: ' whois_info.txt | awk '{print $2}')
 
 echo "E-mail de contato:"
 grep -i 'email:' whois_info.txt
